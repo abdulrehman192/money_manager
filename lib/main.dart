e68ed41a15daf853581dc7fd3cf01c8be92b45fe
Abdul Rehman <mr.abdulrehman.ar@gmail.com>
2022-11-19 14:43:43 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '/views/views.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Money Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.grey,
+      ),
+      home: const SplashView()
+    );
+  }
+}
